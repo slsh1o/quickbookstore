6ab17e6fcb98224be70376ec8d546f3a583dcf77
do-While-on-GitHub <atterdag@zoho.eu>
2019-06-22 23:39:28 +0300

    Initial commit

---
+++ src/main/java/com/provectus/quickbookstore/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.provectus.quickbookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main(Model model) {
+
+        return "main";
+    }
+}
---
+++ src/main/resources/templates/main.ftl
@@ -0,0 +1,7 @@
+<#import "parts/common.ftl" as common>
+
+<@common.main>
+    <div>
+        <h1>Here will be my new project to get my first real job</h1>
+    </div>
+</@common.main>
